59833a92e02059315b4f18199f255b8582bc8396
Rick-TIY <rick@theironyard.com>
2016-08-02 13:41:57 -0600

    confirming my instructions are clear

--- src/main/java/springBootApp/controllers/JspController.java
+++ src/main/java/springBootApp/controllers/JspController.java
@@ -1,10 +1,15 @@
 package springBootApp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springBootApp.entities.UserDao;
 
 @Controller
 public class JspController {
+
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(value="/")
     public String jspIndex() {
