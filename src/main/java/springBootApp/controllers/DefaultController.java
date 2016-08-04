6cbc107987bf8eb1c4213cbcec29cf0c9f99cdfa
Rick-TIY <rick@theironyard.com>
2016-08-04 16:36:10 -0600

    Cleaning up the code

--- src/main/java/springBootApp/controllers/DefaultController.java
+++ src/main/java/springBootApp/controllers/DefaultController.java
@@ -3,9 +3,6 @@ package springBootApp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class DefaultController {
 
